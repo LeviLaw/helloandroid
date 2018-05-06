a565ded4e20d384480c5e5e64c1e288a9d1d9504
LeviLaw <37970253+LeviLaw@users.noreply.github.com>
2018-05-06 17:18:40 +0800

    Initial commit

---
+++ app/src/main/java/com/example/asus/myapplication/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.asus.myapplication;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
+
+
+
+
